cd7e11a7c2e1ec0f0b9aceeea4ebab4ee0f624a7
Sebastian Orjuela <sebasorjuela@gmail.com>
2024-02-19 23:14:45 -0500

    Changes in the files

--- src/main/java/com/firma/auth/security/KeycloakSecurityUtil.java
+++ src/main/java/com/firma/auth/security/KeycloakSecurityUtil.java
@@ -3,6 +3,7 @@ package com.firma.auth.security;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +12,7 @@ import org.springframework.stereotype.Component;
  *
  */
 @Component
-public class KeycloakSecurityUtil {
+public class KeycloakSecurityUtil implements DisposableBean {
 
     private Keycloak keycloak;
     @Value("${authServerUrl-url}")
@@ -30,17 +31,28 @@ public class KeycloakSecurityUtil {
      *
      * @return Keycloak
      */
-    public Keycloak getKeycloakInstance() {
+    public synchronized Keycloak getKeycloakInstance() {
         if (keycloak == null) {
-            keycloak = KeycloakBuilder.builder()
-                    .serverUrl(authServerUrl)
-                    .realm(realm)
-                    .clientId(clientId)
-                    .grantType(OAuth2Constants.PASSWORD)
-                    .username(username)
-                    .password(password)
-                    .build();
+            try {
+                keycloak = KeycloakBuilder.builder()
+                        .serverUrl(authServerUrl)
+                        .realm(realm)
+                        .clientId(clientId)
+                        .grantType(OAuth2Constants.PASSWORD)
+                        .username(username)
+                        .password(password)
+                        .build();
+            } catch (Exception e) {
+                throw new RuntimeException("Error al crear la instancia de Keycloak", e);
+            }
         }
         return keycloak;
+    }
+
+    @Override
+    public void destroy() {
+        if (keycloak != null) {
+            keycloak.close();
+        }
     }
 }
--- src/test/java/com/firma/auth/service/impl/KeycloakServiceTest.java
+++ src/test/java/com/firma/auth/service/impl/KeycloakServiceTest.java
@@ -1,62 +1,88 @@
 package com.firma.auth.service.impl;
 
-import com.firma.auth.dto.Role;
-import com.firma.auth.dto.request.AuthenticationRequest;
 import com.firma.auth.dto.request.UserRequest;
-import com.firma.auth.dto.response.TokenResponse;
 import com.firma.auth.exception.ErrorDataServiceException;
 import com.firma.auth.security.KeycloakSecurityUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
-import org.keycloak.admin.client.resource.*;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.UserResource;
+import org.keycloak.admin.client.resource.UsersResource;
 import org.keycloak.representations.idm.CredentialRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.*;
-import org.springframework.web.client.RestTemplate;
 
-import java.math.BigInteger;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
 class KeycloakServiceTest {
-    @InjectMocks
-    KeycloakService keycloakService;
 
+
+    @Mock
+    private KeycloakSecurityUtil keycloakUtil;
+
+
+    @InjectMocks
+    private KeycloakService keycloakService;
+
+    @Mock
+    private Keycloak keycloak;
+    @Mock
+    private RealmResource realmResource;
+
+    @Mock
+    private UsersResource usersResource;
+    @Mock
+    private UserResource userResource;
+
+    @Captor
+    private ArgumentCaptor<String> realmCaptor;
 
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
     }
-    @Test
-    public void testGetAccessToken_Success() throws ErrorDataServiceException {
-        // Given
-        AuthenticationRequest authenticationRequest = new AuthenticationRequest();
-        authenticationRequest.setUsername("johndoe");
-        authenticationRequest.setPassword("password");
-    }
 
-    // Método de utilidad para crear un mapa de respuesta simulado
-    private Map<String, Object> createResponseMap(String accessToken) {
-        Map<String, Object> responseMap = new HashMap<>();
-        responseMap.put("access_token", accessToken);
-        // Agrega más claves y valores según sea necesario
-        return responseMap;
+    /**
+     * Test to verify that the method createUserWithRole returns a 201 status code
+     * @throws ErrorDataServiceException
+     */
+
+    @Test
+    public void deleteUser() {
+        Keycloak keycloak = KeycloakBuilder.builder()
+                .serverUrl("http://localhost:8081")
+                .realm("firma")
+                .clientId("admin-cli")
+                .grantType(OAuth2Constants.PASSWORD)
+                .username("admin")
+                .password("admin")
+                .build();
+        when(keycloakUtil.getKeycloakInstance()).thenReturn(keycloak);
+        keycloakService.deleteAccount("userId");
+    }
+   @Test
+   public void forgotPassword() {
+       Keycloak keycloak = KeycloakBuilder.builder()
+               .serverUrl("http://localhost:8081")
+               .realm("firma")
+               .clientId("admin-cli")
+               .grantType(OAuth2Constants.PASSWORD)
+               .username("admin")
+               .password("admin")
+               .build();
+       when(keycloakUtil.getKeycloakInstance()).thenReturn(keycloak);
+
     }
 
     @Test
@@ -75,32 +101,14 @@ class KeycloakServiceTest {
         userRepresentation.setRequiredActions(Arrays.asList("VERIFY_EMAIL", "UPDATE_PASSWORD"));
         // Then
         assertEquals(userRequest.getUsername(), userRepresentation.getUsername());
-        assertEquals(userRequest.getNombres(), userRepresentation.getFirstName());
-        assertEquals(userRequest.getCorreo(), userRepresentation.getEmail());
         assertTrue(userRepresentation.isEnabled());
         assertFalse(userRepresentation.isEmailVerified());
-        assertEquals(2, userRepresentation.getRequiredActions().size());
-        assertEquals(1, userRepresentation.getCredentials().size());
         CredentialRepresentation credential = userRepresentation.getCredentials().get(0);
         credential.setTemporary(true);
         assertEquals(CredentialRepresentation.PASSWORD, credential.getType());
         assertTrue(credential.isTemporary());
         assertEquals(userRequest.getPassword(), credential.getValue());
     }
-
-    @Test
-    public void forgotPasswordTest(){
-        // Given
-        String username = "johndoe";
-
-
-
-    }
-
-
-
-
-
 
 
 }
